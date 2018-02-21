89a6475c0276e2d356cb8cef544815d69a7ab476
Teresa Carromeu <teresa.marrafa@hotmail.com>
2018-02-21 20:32:32 +0000

    Created a css file

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 # mandatory import the library
-from flask import Flask
+from flask import Flask, render_template
 
 # create an index route
 app = Flask("my_first_app")
 
 @app.route("/")
 def say_hello():
-    return"hello world!"
+    return render_template("index.html")
 
 #app runs!
 app.run(debug=True)
